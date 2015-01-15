0f36d54aefc351dcb576a59966821696e2b89ebd
Michele Bertoli <michele@berto.li>
2015-01-15 21:28:50 +0000

    footer

--- wp-content/themes/nodejs/footer.php
+++ wp-content/themes/nodejs/footer.php
@@ -1,3 +1,35 @@
+    <div class="footer-block">
+      <div class="content">
+        <footer class="footer">
+          <div class="footer__box footer__box--main">
+            <div class="footer__title">
+              - Node.js -
+            </div>
+            <div class="footer__subtitle">
+              Italian Conference - IV Edition
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/bg-node.png" alt="">
+          </div>
+          <div class="footer__box">
+            <strong>
+              NodejsConfIT
+            </strong> 
+            is proudly organized by 
+            <img src="<?php echo get_template_directory_uri(); ?>/images/webdebs.png" alt="WEBdeBS">
+            <a href="#">
+              www.webdebs.org
+            </a>
+            <a href="#">
+              info@webdebs.org
+            </a>
+            <a href="#">
+              facebook.com/webdebs
+            </a>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <div class="decorations"></div>
     <script src="<?php echo get_template_directory_uri(); ?>/app.js"></script>
     <?php wp_footer(); ?>
   </body>
