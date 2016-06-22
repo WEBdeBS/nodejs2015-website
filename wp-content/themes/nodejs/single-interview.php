2752a969e668a090af19237256ec152d88437a5e
Davide Brognoli <davide.brognoli@gmail.com>
2016-06-22 15:48:59 +0200

    Template interviews

--- wp-content/themes/nodejs/single-interview.php
+++ wp-content/themes/nodejs/single-interview.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
 	<div class="content">
 		<article class="main-page">
-			<div class="title title--main-page col-sm-3">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/title-tickets.png" alt="">
-				Interviews
-			</div>
-			<div class="news-container col-sm-6">
+			<div class="news-container col-sm-9">
 	<?php 
 		if (have_posts()) {
 			while(have_posts()){
